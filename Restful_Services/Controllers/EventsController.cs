86e70349f2138dd1afb4352c37917d9d312014ec
Venkateshwaran Saravanakumar <venkateshwaran.s@syncfusion.com>
2023-05-08 08:52:24 +0530

    Documentation(ES-104982):Updated samples for RestfulServices in GitHub samples

---
+++ Restful_Services/Controllers/EventsController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestfulServices.Models;
+using Microsoft.AspNetCore.OData;
+using Microsoft.AspNetCore.OData.Routing.Controllers;
+using Microsoft.AspNetCore.OData.Query;
+using Microsoft.AspNetCore.OData.Formatter;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RestfulServices.Controllers
+{
+    //[Route("api/")]
+    //[ApiController]
+    public class EventsController : ODataController
+    {
+        private ScheduleDataContext _db;
+        // GET: api/<EventsController>
+        public EventsController(ScheduleDataContext context)
+        {
+            _db = context;
+            if (context.EventsData.Count() == 0)
+            {
+                foreach (var b in DataSource.GetEvents())
+                {
+                    context.EventsData.Add(b);
+                }
+                context.SaveChanges();
+            }
+
+        }
+        [HttpGet]
+        [EnableQuery]
+        public IActionResult Get()
+        {
+            return Ok(_db.EventsData);
+        }
+
+        //[HttpGet("{id}")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        public async Task Post([FromBody] EventData events)
+        {
+            _db.EventsData.Add(events);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task Put([FromODataUri] int key, [FromBody] EventData events)
+        {
+            var entity = await _db.EventsData.FindAsync(events.Id);
+            if (entity != null)
+            {
+                _db.Entry(entity).CurrentValues.SetValues(events);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task Patch([FromODataUri] int key, [FromBody] EventData events)
+        {
+            var entity = await _db.EventsData.FindAsync(key);
+            //events.Patch(entity);
+            if (entity != null)
+            {
+                _db.Entry(entity).CurrentValues.SetValues(events);
+                await _db.SaveChangesAsync();
+            }
+        }
+
+        public async Task Delete([FromODataUri] int key)
+        {
+            var od = _db.EventsData.Find(key);
+            if (od != null)
+            {
+                _db.EventsData.Remove(od);
+                await _db.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Restful_Services/Models/DataSource.cs
@@ -0,0 +1,115 @@
+﻿using RestfulServices.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulServices.Models
+{
+    public static class DataSource
+    {
+        private static IList<EventData>? _eventsData { get; set; }
+
+        public static IList<EventData> GetEvents()
+        {
+            if (_eventsData != null)
+            {
+                return _eventsData;
+            }
+
+            _eventsData = new List<EventData>();
+
+            EventData related = new EventData
+            {
+                Id = 1,
+                Subject = "Explosion of Betelgeuse Star",
+                Location = "Space Centre USA",
+                StartTime = new DateTime(2020, 1, 5, 9, 30, 0),
+                EndTime = new DateTime(2020, 1, 5, 11, 0, 0)
+            };
+            _eventsData.Add(related);
+
+            EventData events = new EventData
+            {
+                Id = 2,
+                Subject = "Thule Air Crash Report",
+                Location = "Newyork City",
+                StartTime = new DateTime(2020, 1, 6, 12, 0, 0),
+                EndTime = new DateTime(2020, 1, 6, 14, 0, 0)
+            };
+            _eventsData.Add(events);
+
+            events = new EventData
+            {
+                Id = 3,
+                Subject = "Blue Moon Eclipse",
+                Location = "Space Centre USA",
+                StartTime = new DateTime(2020, 1, 7, 9, 30, 0),
+                EndTime = new DateTime(2020, 1, 7, 11, 0, 0)
+            };
+            _eventsData.Add(events);
+
+            events = new EventData
+            {
+                Id = 4,
+                Subject = "Meteor Showers in 2018",
+                Location = "Space Centre USA",
+                StartTime = new DateTime(2020, 1, 8, 13, 0, 0),
+                EndTime = new DateTime(2020, 1, 8, 14, 30, 0)
+            };
+            _eventsData.Add(events);
+
+            events = new EventData
+            {
+                Id = 5,
+                Subject = "Milky Way as Melting pot",
+                Location = "Space Centre USA",
+                StartTime = new DateTime(2020, 1, 9, 12, 0, 0),
+                EndTime = new DateTime(2020, 1, 9, 14, 0, 0)
+            };
+            _eventsData.Add(events);
+
+            events = new EventData
+            {
+                Id = 6,
+                Subject = "Mysteries of Bermuda Triangle",
+                Location = "Bermuda",
+                StartTime = new DateTime(2020, 1, 9, 9, 30, 0),
+                EndTime = new DateTime(2020, 1, 9, 11, 0, 0)
+            };
+            _eventsData.Add(events);
+
+            events = new EventData
+            {
+                Id = 7,
+                Subject = "Glaciers and Snowflakes",
+                Location = "Himalayas",
+                StartTime = new DateTime(2020, 1, 10, 11, 0, 0),
+                EndTime = new DateTime(2020, 1, 10, 12, 30, 0)
+            };
+            _eventsData.Add(events);
+
+            events = new EventData
+            {
+                Id = 8,
+                Subject = "Life on Mars",
+                Location = "Space Centre USA",
+                StartTime = new DateTime(2020, 1, 11, 9, 0, 0),
+                EndTime = new DateTime(2020, 1, 11, 10, 0, 0)
+            };
+            _eventsData.Add(events);
+
+            events = new EventData
+            {
+                Id = 9,
+                Subject = "Alien Civilization",
+                Location = "Space Centre USA",
+                StartTime = new DateTime(2020, 1, 13, 11, 0, 0),
+                EndTime = new DateTime(2020, 1, 13, 13, 0, 0)
+            };
+            _eventsData.Add(events);
+
+            return _eventsData;
+        }
+    }
+}
