dda2d28e650dcc1d990029cde21cb4c8c1cc95c1
Jai Chandran Ravichandran <jaichandran.r@syncfusion.com>
2026-03-06 11:18:57 +0530

    Sample Blazor Entity Framework

--- Restful_Services/Models/DataSource.cs
+++ Restful_Services/Models/DataSource.cs
@@ -8,7 +8,7 @@ namespace RestfulServices.Models
 {
     public static class DataSource
     {
-        private static IList<EventData>? _eventsData { get; set; }
+        public static IList<EventData>? _eventsData { get; set; }
 
         public static IList<EventData> GetEvents()
         {
@@ -24,8 +24,8 @@ namespace RestfulServices.Models
                 Id = 1,
                 Subject = "Explosion of Betelgeuse Star",
                 Location = "Space Centre USA",
-                StartTime = new DateTime(2020, 1, 5, 9, 30, 0),
-                EndTime = new DateTime(2020, 1, 5, 11, 0, 0)
+                StartTime = new DateTime(2026, 1, 5, 9, 30, 0),
+                EndTime = new DateTime(2026, 1, 5, 11, 0, 0)
             };
             _eventsData.Add(related);
 
@@ -34,8 +34,8 @@ namespace RestfulServices.Models
                 Id = 2,
                 Subject = "Thule Air Crash Report",
                 Location = "Newyork City",
-                StartTime = new DateTime(2020, 1, 6, 12, 0, 0),
-                EndTime = new DateTime(2020, 1, 6, 14, 0, 0)
+                StartTime = new DateTime(2026, 1, 6, 12, 0, 0),
+                EndTime = new DateTime(2026, 1, 6, 14, 0, 0)
             };
             _eventsData.Add(events);
 
@@ -44,8 +44,8 @@ namespace RestfulServices.Models
                 Id = 3,
                 Subject = "Blue Moon Eclipse",
                 Location = "Space Centre USA",
-                StartTime = new DateTime(2020, 1, 7, 9, 30, 0),
-                EndTime = new DateTime(2020, 1, 7, 11, 0, 0)
+                StartTime = new DateTime(2026, 1, 7, 9, 30, 0),
+                EndTime = new DateTime(2026, 1, 7, 11, 0, 0)
             };
             _eventsData.Add(events);
 
@@ -54,8 +54,8 @@ namespace RestfulServices.Models
                 Id = 4,
                 Subject = "Meteor Showers in 2018",
                 Location = "Space Centre USA",
-                StartTime = new DateTime(2020, 1, 8, 13, 0, 0),
-                EndTime = new DateTime(2020, 1, 8, 14, 30, 0)
+                StartTime = new DateTime(2026, 1, 8, 13, 0, 0),
+                EndTime = new DateTime(2026, 1, 8, 14, 30, 0)
             };
             _eventsData.Add(events);
 
@@ -64,8 +64,8 @@ namespace RestfulServices.Models
                 Id = 5,
                 Subject = "Milky Way as Melting pot",
                 Location = "Space Centre USA",
-                StartTime = new DateTime(2020, 1, 9, 12, 0, 0),
-                EndTime = new DateTime(2020, 1, 9, 14, 0, 0)
+                StartTime = new DateTime(2026, 1, 9, 12, 0, 0),
+                EndTime = new DateTime(2026, 1, 9, 14, 0, 0)
             };
             _eventsData.Add(events);
 
@@ -74,8 +74,8 @@ namespace RestfulServices.Models
                 Id = 6,
                 Subject = "Mysteries of Bermuda Triangle",
                 Location = "Bermuda",
-                StartTime = new DateTime(2020, 1, 9, 9, 30, 0),
-                EndTime = new DateTime(2020, 1, 9, 11, 0, 0)
+                StartTime = new DateTime(2026, 1, 9, 9, 30, 0),
+                EndTime = new DateTime(2026, 1, 9, 11, 0, 0)
             };
             _eventsData.Add(events);
 
@@ -84,8 +84,8 @@ namespace RestfulServices.Models
                 Id = 7,
                 Subject = "Glaciers and Snowflakes",
                 Location = "Himalayas",
-                StartTime = new DateTime(2020, 1, 10, 11, 0, 0),
-                EndTime = new DateTime(2020, 1, 10, 12, 30, 0)
+                StartTime = new DateTime(2026, 1, 10, 11, 0, 0),
+                EndTime = new DateTime(2026, 1, 10, 12, 30, 0)
             };
             _eventsData.Add(events);
 
@@ -94,8 +94,8 @@ namespace RestfulServices.Models
                 Id = 8,
                 Subject = "Life on Mars",
                 Location = "Space Centre USA",
-                StartTime = new DateTime(2020, 1, 11, 9, 0, 0),
-                EndTime = new DateTime(2020, 1, 11, 10, 0, 0)
+                StartTime = new DateTime(2026, 1, 11, 9, 0, 0),
+                EndTime = new DateTime(2026, 1, 11, 10, 0, 0)
             };
             _eventsData.Add(events);
 
@@ -104,8 +104,8 @@ namespace RestfulServices.Models
                 Id = 9,
                 Subject = "Alien Civilization",
                 Location = "Space Centre USA",
-                StartTime = new DateTime(2020, 1, 13, 11, 0, 0),
-                EndTime = new DateTime(2020, 1, 13, 13, 0, 0)
+                StartTime = new DateTime(2026, 1, 13, 11, 0, 0),
+                EndTime = new DateTime(2026, 1, 13, 13, 0, 0)
             };
             _eventsData.Add(events);
 
